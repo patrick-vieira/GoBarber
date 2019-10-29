93764a21178acf20a44f69f8c9661e50ff0703e0
patrick-vieira <pdsv88@gmail.com>
2019-10-28 22:55:48 -0300

    list/read notification

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import FileController from './app/controllers/FileController';
 
@@ -31,6 +32,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedules', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
